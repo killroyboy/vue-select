d6980e9751dd3873d6006a1f843c8d9a8e7e89e9
Dan Wilson <killroyboy@gmail.com>
2019-01-09 21:04:30 -0700

    update the actual usage of the dropdown-toggle class

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -311,7 +311,7 @@
 
 <template>
   <div :dir="dir" class="dropdown v-select" :class="dropdownClasses">
-    <div ref="toggle" @mousedown.prevent="toggleDropdown" class="dropdown-toggle">
+    <div ref="toggle" @mousedown.prevent="toggleDropdown" class="v-select-dropdown-toggle">
 
       <div class="vs__selected-options" ref="selectedOptions">
         <slot v-for="option in valueAsArray" name="selected-option-container"
